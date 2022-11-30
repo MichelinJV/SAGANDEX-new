d1f8fe3498664aafbbf7c106a998edecd0bd784b
MichelinJV <michelinjv@gmail.com>
2022-11-30 10:59:17 -0300

    comitando

---
+++ SaganDex/Models/ErrorViewModel.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace SaganDex.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        
+
+        
+
+    }
+}
